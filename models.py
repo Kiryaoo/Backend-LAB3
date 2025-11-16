53d28a1c0f2c7d0f0416b8bbfb29e4acb1c4f347
Kyryl <kirillka.k@gmail.com>
2025-11-16 03:15:03 +0200

    Updated models

--- models.py
+++ models.py
@@ -1,17 +1,23 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 from datetime import datetime
 
 class User(BaseModel):
     id: int
-    name: str
+    name: str = Field(..., min_length=2, max_length=50)
 
 class Category(BaseModel):
     id: int
-    title: str
-
+    title: str = Field(..., min_length=2, max_length=50)
 class Record(BaseModel):
     id: int
-    user_id: int
-    category_id: int
-    amount: float
+    user_id: int = Field(..., ge=1)
+    category_id: int = Field(..., ge=1)
+    amount: float = Field(..., gt=0)
     timestamp: datetime
+
+    @field_validator("timestamp")
+    @classmethod
+    def validate_date(cls, value: datetime) -> datetime:
+        if value > datetime.now():
+            raise ValueError("Timestamp cannot be in the future")
+        return value
